21b9fef9d88403a2badf7e523a6578c48befe930
Valeriia <valeriia.vp1@yandex.ru>
2026-04-26 22:13:51 +0200

    add Vimeo integration test

---
+++ src/test/java/VimeoApiClientIntegrationTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.mephi.case2.api.client.VimeoApiClient;
+import ru.mephi.case2.db.entity.Platform;
+import ru.mephi.case2.http.HttpApiClient;
+import ru.mephi.case2.log.BackendLogger;
+import ru.mephi.case2.util.ApiConfig;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class VimeoApiClientIntegrationTest {
+
+    private VimeoApiClient client;
+    private final String apiUrl = ApiConfig.getApiUrl(Platform.VIMEO);
+    private final String token = ApiConfig.getApiToken(Platform.VIMEO);
+
+    @BeforeEach
+    void setup() {
+        client = new VimeoApiClient(HttpApiClient.getInstance(), apiUrl, token);
+    }
+
+    @Test
+    void getRealStats() {
+        assumeTrue(token != null && !token.isBlank(), "Token is not set");
+
+        String videoUrl = "https://vimeo.com/76979871";
+        Long views = client.getViewsStats(videoUrl);
+
+        assertTrue(views != null && views > 0,
+                "Expected views > 0, but got " + views);
+
+        BackendLogger.log("[TEST SUCCESS] views: " + views);
+    }
+}
